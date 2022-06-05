38005cddbf727b5a227dab8db9ddd6220646ba6b
Branden Palmer <brandenpalmer08@gmail.com>
2022-06-05 16:09:32 +0700

    add all pics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import "./App.scss";
 
 function App() {
+  const memoryImages = [];
+
+  function importAll(r) {
+    r.keys().forEach((key) => memoryImages.push(r(key)));
+  }
+
+  importAll(require.context("./imgs/alpha/", false, /svg$/));
+
+  function renderAllImages() {
+    return (
+      <div>
+        {memoryImages.map((image) => {
+          return <img src={image} alt="" />;
+        })}
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="title">
-          Test Your Memory
-        </h1>
+        <h1 className="title">Test Your Memory</h1>
+        {renderAllImages()}
       </header>
     </div>
   );
